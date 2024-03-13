3dd37f292923aaa37554251967e4b217f833fdf7
dilli <dilli@howdy.do>
2024-03-13 21:59:00 +0530

    new setup

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var joke_container = document.getElementById("joke");
+var btn = document.getElementById("btn");
+joke_container.textContent = 'Dillibabu';
+btn.addEventListener('click',getjoke);
+
+
+function getjoke(){
+    alert()
+    document.getElementById("joke").innerHTML = 'dilli babu'
+}
